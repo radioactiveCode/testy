2e076fb538313ea3b1f29d4d1f68effd4b2b285a
radioactiveCode <101395303+radioactiveCode@users.noreply.github.com>
2022-05-26 10:01:15 +0200

    Add files via upload

---
+++ kafelki.js
@@ -0,0 +1,28 @@
+﻿
+//alert("strona została załadowana");
+
+function klikD()
+{
+    //alert("wywołanie funkcji DODAJ");
+    const kafelek = document.createElement('div');
+    kafelek.setAttribute("class","guzik");
+    const wstawka = document.getElementById("kontener");
+    wstawka.appendChild(kafelek);
+}
+
+function klikU()
+{
+    //alert("wywołanie funkcji USUŃ");
+    //const kafelek = document.createElement('div');
+    //kafelek.setAttribute("class","guzik");
+    const kasuj = document.getElementById("kontener");
+    kasuj.removeChild(kasuj.lastElementChild);
+}
+		
+var elemD=document.getElementById("pasek");
+var elemU=document.getElementById("pasek1");
+
+elemD.addEventListener('click', klikD);
+elemU.addEventListener('click', klikU);
+
+       
